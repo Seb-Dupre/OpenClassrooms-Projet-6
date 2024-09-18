080f3cb0cd72824c2791856fc4e9db4042bb1fc8
Dupre Sebastien <sebastien.dupre.42@gmail.com>
2024-09-18 22:11:50 +0200

    partie 1 et 2 du js terminé, partie 3 en cours

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,27 +1,110 @@
 const gallery = document.querySelector(".gallery")
+const filters = document.querySelector(".filters")
 
 
-
-/*recupere*/
-async function fetchData() {
+/*Recuperation des works */
+async function fetchWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
   }
-  fetchData();
+  fetchWorks();
   
-  /*Affichage et creation des elements*/
-  async function displayData() {
-    const arrayWorks = await fetchData();
-    arrayWorks.forEach((data) => {
+
+  /*Affichage et creation des works*/
+  async function displayWorks() {
+    const works = await fetchWorks();
+    works.forEach((work) => {
+      createWorks(work);
+    });
+  }
+  displayWorks();
+
+  function createWorks(work) {
       const figure = document.createElement("figure");
       const img = document.createElement("img");
       const figcaption = document.createElement("figcaption");
-      img.src = data.imageUrl;
-      figcaption.textContent = data.title;
+      img.src = work.imageUrl;
+      figcaption.textContent = work.title;
       figure.appendChild(img);
       figure.appendChild(figcaption);
       gallery.appendChild(figure);
-    });
+    };
+  
+
+  /* categories */
+
+  /* recupere les categories */
+  async function fetchCategories() {
+    const categories = localStorage.getItem("categories")
+    if(categories){
+      return JSON.parse(categories)
+    }
+    const response = await fetch("http://localhost:5678/api/categories"); 
+    const json = await response.json();
+    localStorage.setItem("categories", JSON.stringify(json))
+    return json;
+    
   }
   
-  displayData();
+  fetchCategories();
+  /* créer et affiche les boutons de categories */
+
+  async function displayCategory() {
+    const categories = await fetchCategories();
+    categories.forEach((category) => {
+      const divBtn = document.createElement("div");
+      const btn = document.createElement("button");
+      divBtn.classList.add("filter") 
+      btn.className = "filter__button";
+      btn.textContent = category.name;
+      btn.id = category.id;
+      filters.appendChild(divBtn);
+      divBtn.appendChild(btn);
+    })}
+    displayCategory()
+    
+    /* filtre */
+
+    async function categoryFilter() {
+      const works = await fetchWorks();
+      const buttons = document.querySelectorAll(".filter__button")
+
+      let index = 0;
+      buttons.forEach(button => {
+        button.addEventListener("click",(e)=>{
+          buttons[index].classList.remove('filter__button-selected');
+          buttonId = e.target.id;
+          gallery.innerHTML ="";
+          if(buttonId !== "0" ){
+            const filteredWorks = works.filter((work)=>{
+              return work.categoryId == buttonId;
+            });
+            filteredWorks.forEach(work => {
+              createWorks(work);  
+              buttons[buttonId].classList.add('filter__button-selected');
+              index = buttonId;
+            });
+          } 
+          else {displayWorks();
+            index = 0;
+            buttons[index].classList.add('filter__button-selected');
+
+          }
+        })       
+      });
+    }
+    categoryFilter();
+//rajouté les categories dans localStorage
+
+
+const loged = window.localStorage.loged;
+const logout = document.querySelector(".logout");
+console.log(loged)
+if (loged == "true") {
+  logout.textContent = "logout";
+  logout.addEventListener("click", () => {
+    logout.href = 'index.html';
+    localStorage.removeItem("token");
+    window.localStorage.loged = false;
+  });
+}
---
+++ FrontEnd/login.js
@@ -0,0 +1,47 @@
+
+const email = document.querySelector("form #email");
+const password = document.querySelector("form #password");
+const form = document.querySelector("form");
+const errorMessage = document.querySelector(".login p");
+
+
+async function login(login, pass) {
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login",{method:"post", body:JSON.stringify({email:login, password:pass}),headers: {
+            "Content-Type": "application/json",
+          }
+    });
+        return await response;
+    }
+    
+    catch (error) {
+        console.log("ERROR")
+    } 
+    console.log(response)
+}
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const response = await login(email.value , password.value)
+    console.log(response.status)
+
+    
+    if (response.status === 200){
+
+        const json = await response.json(); 
+        localStorage.setItem("token", json.token);
+        window.localStorage.loged = true;
+        window.location.href = "index.html";
+    }
+    else{
+        errorMessage.textContent ='email ou mot de passe est incorrect. '
+    }
+})
+/*login email and pwd : "sophie.bluel@test.tld" , "S0phie" */ 
+/*
+console.log(localStorage.getItem("token"))
+if(localStorage.getItem("token")){
+    window.location.href = "index.html";
+}
+*/
+//localStorage.removeItem("token")
